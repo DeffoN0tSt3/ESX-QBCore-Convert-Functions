89be272a48bc3bd187092d19633f2a1305ffffb2
n0tste3 <74063176+DeffoN0tSt3@users.noreply.github.com>
2022-07-18 23:29:59 +0100

    Create fetchStatus.lua

---
+++ fetchStatus.lua
@@ -0,0 +1,47 @@
+ESX.RegisterServerCallback("system:fetchStatus", function(source, cb)
+    local src = source
+    local user = ESX.GetPlayerFromId(src)
+
+
+    local fetch = [[
+         SELECT
+              skills
+         FROM
+              users
+         WHERE
+              identifier = @identifier
+    ]]
+
+    MySQL.Async.fetchScalar(fetch, {
+         ["@identifier"] = user.identifier
+
+    }, function(status)
+
+         if status ~= nil then
+              cb(json.decode(status))
+         else
+              cb(nil)
+         end
+
+    end)
+end)
+
+-->
+
+QBCore.Functions.CreateCallback('system:fetchStatus', function(source, cb)
+    local Player = QBCore.Functions.GetPlayer(source)
+
+     if Player then
+           exports['ghmattimysql']:execute('SELECT skills FROM players WHERE citizenid = @citizenid', {
+               ['@citizenid'] = Player.PlayerData.citizenid
+          }, function(status)
+              if status ~= nil then
+                   cb(json.decode(status))
+              else
+                   cb(nil)
+              end
+          end)
+     else
+          cb()
+     end
+end)
