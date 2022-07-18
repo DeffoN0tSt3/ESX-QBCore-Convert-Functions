923e3e4bc620ac5424e029429bc97bd8fcedf4f6
n0tste3 <74063176+DeffoN0tSt3@users.noreply.github.com>
2022-07-19 00:03:00 +0100

    Create SERVERSIDE-Functions.lua

---
+++ SERVERSIDE-Functions.lua
@@ -0,0 +1,34 @@
+
+QBCore.Functions.ExecuteSql = function(wait, query, cb) 
+
+QBCore.Functions.GetIdentifier = function(source, idtype) 
+
+QBCore.Functions.GetSource = function(identifier)
+
+QBCore.Functions.GetPlayer = function(source)
+
+QBCore.Functions.GetPlayerByCitizenId = function(citizenid)
+
+QBCore.Functions.GetPlayerByPhone = function(number)
+
+QBCore.Functions.GetPlayers = function()
+
+QBCore.Functions.CreateCallback = function(name, cb)
+
+QBCore.Functions.TriggerCallback = function(name, source, cb, ...)
+
+QBCore.Functions.CreateUseableItem = function(item, cb)
+
+QBCore.Functions.CanUseItem = function(item)
+
+QBCore.Functions.UseItem = function(source, item)
+
+QBCore.Functions.AddPermission = function(source, permission)
+
+QBCore.Functions.RemovePermission = function(source)
+
+QBCore.Functions.HasPermission = function(source, permission)
+
+QBCore.Functions.GetPermission = function(source)
+
+QBCore.Functions.IsPlayerBanned = function (source) 
