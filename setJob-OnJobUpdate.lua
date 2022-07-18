3b65b910cae705a26c5b297076816551d10d4b04
n0tste3 <74063176+DeffoN0tSt3@users.noreply.github.com>
2022-07-18 23:16:41 +0100

    Update setJob-OnJobUpdate.lua

--- setJob-OnJobUpdate.lua
+++ setJob-OnJobUpdate.lua
@@ -1,7 +1,11 @@
 RegisterNetEvent('esx:setJob')
-AddEventHandler('esx:setJob')
+AddEventHandler('esx:setJob', function(job)
+    PlayerData.job = job
+end)
 
 -->
 
-RegisterNetEvent('QBCore:Client:OnJobUptade')
-AddEventHandler('QBCore:Client:OnJobUptade')
+RegisterNetEvent('QBCore:Client:OnJobUpdate')
+AddEventHandler('QBCore:Client:OnJobUpdate', function(job)
+    PlayerData.job = job
+end)
