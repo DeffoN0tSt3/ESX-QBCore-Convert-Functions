7b481405194740cd425de79706a08e7fc402bad1
n0tste3 <74063176+DeffoN0tSt3@users.noreply.github.com>
2022-07-18 23:57:05 +0100

    Create EVENTS.lua

---
+++ EVENTS.lua
@@ -0,0 +1,7 @@
+esx:getSharedObject -> QBCore:GetObject
+esx:setJob -> QBCore:Client:OnJobUpdate
+esx:onPlayerSpawn -> QBCore:Client:OnPlayerLoaded
+playerSpawned -> QBCore:Client:OnPlayerLoaded (spawnmanager compatibility)
+esx:addInventoryItem -> QBCore:Server:AddItem
+esx:removeInventoryItem -> QBCore:Server:RemoveItem
+esx:useItem -> QBCore:Server:UseItem
