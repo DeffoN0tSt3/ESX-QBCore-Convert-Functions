6f16b3a0ee5625ae798fcc7f3e0c51b756b0cfae
n0tste3 <74063176+DeffoN0tSt3@users.noreply.github.com>
2022-07-18 23:43:53 +0100

    Update MathRound.lua

--- MathRound.lua
+++ MathRound.lua
@@ -6,6 +6,10 @@ print ('n0tst3:' .. ESX.Math.Round(n0tst3, 1))
 
 -->
 
+QBCore.Functions.MathRound(GetVehicleBodyHealth(vehicle), 1),
+--
+QBCore.Functions.MathRound()
+
 local n0tst3 = 1337.1
 QBCore.Functions.MathRound(n0tst3, numberDecimals)
 print ('n0tst3:' .. n0tst3)
