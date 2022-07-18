528c657bf4e9b50b7a8cbe5a95fe44ca9fbdb4ae
n0tste3 <74063176+DeffoN0tSt3@users.noreply.github.com>
2022-07-18 23:57:32 +0100

    Create XPLAYER.lua

---
+++ XPLAYER.lua
@@ -0,0 +1,13 @@
+xPlayer.removeWeaponComponent -> xPlayer.Functions.RemoveItem (component name)
+xPlayer.setAccountMoney -> xPlayer.Functions.SetMoney (account)
+xPlayer.setCoords -> None (used for teleporting)
+xPlayer.setInventoryItem -> xPlayer.Functions.AddItem (item name)
+xPlayer.setJob -> xPlayer.Functions.SetJob
+xPlayer.setMaxWeight -> None (It is set in qb-core config) 
+xPlayer.setMoney -> xPlayer.Functions.SetMoney
+xPlayer.setName -> None (dont bother)
+xPlayer.setWeaponTint -> None (qb-weapons does this)
+xPlayer.showHelpNotification -> TriggerClientEvent('QBCore:Notify')
+xPlayer.showNotification -> TriggerClientEvent('QBCore:Notify')
+xPlayer.triggerEvent -> None (dont bother)
+xPlayer.updateCoords -> None (dont bother) 
