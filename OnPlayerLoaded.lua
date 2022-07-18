ad385b582928ae2f83efa7d61975cb0b1c5e86b4
n0tste3 <74063176+DeffoN0tSt3@users.noreply.github.com>
2022-07-18 22:54:16 +0100

    Update OnPlayerLoaded.lua

--- OnPlayerLoaded.lua
+++ OnPlayerLoaded.lua
@@ -1,2 +1,7 @@
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded',
+  
+-->
+  
 RegisterNetEvent('QBCore:Client:OnPlayerLoaded')
 AddEventHandler('QBCore:Client:OnPlayerLoaded',
